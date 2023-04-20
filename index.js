f95a410813d64a7bc65c74d0a9e5f2583960ccf7
thanh.do <jennytd0201@gmail.com>
2023-04-19 21:48:18 -0700

    hw10/tut14

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+//Use Multer
+const multer = require("multer");
+const upload = multer();
+
 //Inlude the dotenv package
 require('dotenv').config();
 
@@ -45,5 +49,59 @@ app.get("/", (req, res) => {
           message: message,
           model : model
       });
+  });
+});
+
+app.get("/input", (req, res) => {
+  res.render("input");
+});
+
+app.post("/input",  upload.single('filename'), (req, res) => {
+   if(!req.file || Object.keys(req.file).length === 0) {
+       message = "Error: Import file not uploaded";
+       return res.send(message);
+   };
+   //Read file line by line, inserting records
+   const buffer = req.file.buffer; 
+   const lines = buffer.toString().split(/\r?\n/);
+
+   lines.forEach(line => {
+        //console.log(line);
+        product = line.split(",");
+        //console.log(product);
+        const sql = "INSERT INTO PRODUCT(prod_id, prod_name, prod_desc, prod_price) VALUES ($1, $2, $3, $4)";
+        pool.query(sql, product, (err, result) => {
+            if (err) {
+                console.log(`Insert Error.  Error message: ${err.message}`);
+            } else {
+                console.log(`Inserted successfully`);
+            }
+       });
+   });
+   message = `Processing Complete - Processed ${lines.length} records`;
+   res.send(message);
+});
+
+app.get("/output", (req, res) => {
+  var message = "";
+  res.render("output", { message: message });
+});
+
+app.post("/output", (req, res) => {
+  const sql = "SELECT * FROM PRODUCT ORDER BY PROD_ID";
+  pool.query(sql, [], (err, result) => {
+    var message = "";
+    if (err) {
+      message = `Error - ${err.message}`;
+      res.render("output", { message: message })
+    } else {
+      var output = "";
+      result.rows.forEach(product => {
+        output += `${product.prod_id},${product.prod_name},${product.prod_desc},${product.prod_price}\r\n`;
+      });
+      res.header("Content-Type", "text/csv");
+      res.attachment("export.csv");
+      return res.send(output);
+    };
   });
 });
